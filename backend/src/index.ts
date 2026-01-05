12393e94f820f050f25c2aa7d74604d554b43356
Abhay Chouhan <abhaychouhan0503@gmail.com>
2026-01-06 01:17:14 +0530

    routes and controllers

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,11 @@ import express from "express";
 import { ENV } from "./config/env";
 import cors from "cors";
 import { clerkMiddleware } from '@clerk/express'
+
+import userRoutes from "./routes/userRoutes";
+import productRoutes from "./routes/productRoutes";
+import commentRoutes from "./routes/commentRoutes";
+
 const app = express();
 
 app.use(cors({ origin: ENV.FRONTEND_URL, credentials: true }));
@@ -20,6 +25,10 @@ app.get("/", (req, res) => {
     },
   });
 });
+
+app.use("/api/users", userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/comments", commentRoutes);
 app.listen(ENV.PORT, () => console.log("Server is up and running on PORT:",ENV.PORT));
 
 
